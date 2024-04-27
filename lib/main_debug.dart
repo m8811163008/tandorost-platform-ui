d3cb14b91af2e869abc8ed9189c2765d74a1a719
Milad Hesami <m8811163008@gmail.com>
2024-04-27 21:00:42 +0330

    update build number and road map

--- lib/main_debug.dart
+++ lib/main_debug.dart
@@ -35,67 +35,64 @@ void main() async {
   );
 }
 
-// [x]health repository [3h]
-// [x]وضعیت فعلی شما در پروفایل[2h]
-// [x] authentication repostiory [4h]
-// [x] user role for widget tree and then navigation then widgets, create authention widget with stream [3h]
-// [x]create fading text of current stiation in profiel [1h]
-// [x]add recharge subscription or add subscription button [1h]
-// [x]then after opening profile if it is the logged in user then should authenticate with bazzar and can see setting for guest user [1h]
-// [x]add exercise volume day in selected food list banner for subscribers[1h]
-// [x]chnage the widget of banner for calorie (leanear indicator, and update pie chart and labels) for subscribers[1h]
-// [x]expantion tile of considrations with sticky header in selected food list [3h]
-// [x]in app purchase for 1 month subscription and 3 months [6h]
+// refactor
+// error handling of forms for example UpsertFoodBottomSheet
 // add update
 // add support with insta page and website and phone number [تماس با پشتیبانی]
+// sell in bazzar
 // check debug on deploy
 // build and deploy[2h]
-// sell
-// fasting
-
-
-
-// change the pire chart and text in selected foods list
-// news feed like myfitnesspal
-// add tips at main routes and add phycology of writing calorie and its telorance and perception of food calories(in free)
-// add update
+// can not emit state after calling close!
+//
 // hold to create recipe from list and handle name conflict
-// [x]banner for paied user
+// add comment 
+// add next month and last month to date picker in initilize profile wizard date picker
+// add the line indicator chart and text in selected foods list
+// toggle charts type in setting
+// edit profile
+// fasting
+// add general sport in lose weight with 62 and 63% of VO2max, how to calculate vo2 max? optimum speed for weight lose
+// handle create a new food and name conflict in creating new food in creating new food from selectedFoods
+//
 // sppeed up create food when in search field in main page we search food then it goes add new food but did not hold the state for back to add food
+// add chat gpt for food values
+// add chat gpt for recipe
+// add tips at main routes and add phycology of writing calorie and its telorance and perception of food calories(in free)
+// show double weight in chart in profile
+// create outlined icons
+//
+// recalculate
+// news feed like myfitnesspal
+// learning material for which food is for weight lose(آموزش خوراکی های کاهش وزن را بشناسید یا همچین چیزی)
+// add unit of measurments modifiable list 
+// show pictures in profile section , optimize to share on social
+// internationalization
+// body building and fitness platform
+// store in Iran
+// fix paragraph consideration with input chips in descriptions and UI design
+// add change color in setting
+// add magnifier and font size setting and density instaed of current scale factor 1.5
+
+
+// [x] health repository [3h]
+// [x] وضعیت فعلی شما در پروفایل[2h]
+// [x] authentication repostiory [4h]
+// [x] user role for widget tree and then navigation then widgets, create authention widget with stream [3h]
+// [x] create fading text of current stiation in profiel [1h]
+// [x] add recharge subscription or add subscription button [1h]
+// [x] then after opening profile if it is the logged in user then should authenticate with bazzar and can see setting for guest user [1h]
+// [x] add exercise volume day in selected food list banner for subscribers[1h]
+// [x] chnage the widget of banner for calorie (leanear indicator, and update pie chart and labels) for subscribers[1h]
+// [x] expantion tile of considrations with sticky header in selected food list [3h]
+// [x] in app purchase for 1 month subscription and 3 months [6h]
+// [x] banner for paied user
 // [x] hold the state of selected food for unit of measurement 
 // [x] add new food when no food found
 // [x] simple profile for bazzar login
 // [x] user agreement 
 // [x] add form of energy and macro nutrition requirements
-// show double weight in chart in profile
-// suggest learning and sport and some nutrition considiration
-// learning material for which food is for weight lose(آموزش خوراکی های کاهش وزن را بشناسید یا همچین چیزی)
-// add comment 
 // [x] add FAB to list of selected food
 // [x] show how to use counters or expand counter for now to use thumb
-// handle create a new food and name conflict in creating new food in creating new food from selectedFoods
-// edit profile
 // [x] alert of very high rate
-// create outlined icons
-// refactor text fields to stateless
-// add dashboard
 // [x] reset database and ask do you want restore default foods
-// clean streams in blocs, make them same shape or something like that
-// sort food base on calorie and carbohydrate and fat in selected foods list
-// add unit of measurments modifiable list 
-// wellcome screen
-// add chat gpt for food and recepies
-// show pictures in profile section , optimize to share on social
-// recalculate
-// error handling of forms for example UpsertFoodBottomSheet
-// animation to text field of first page
-// add next month and last month to date picker in initilize profile wizard date picker
-// add magnifier and font size setting and density instaed of current scale factor 1.5
-
 // [x] initalize data storage from data storage layer then expose initialize to repositories so they can reset or initialize when needed
-
-// add change color in setting
-// add general sport in lose weight with 62 and 63% of VO2max, how to calculate vo2 max? optimum speed for weight lose
-// fix paragraph in descriptions
-// consideration with input chips
-// banner in food selection for labels , use leanear instead of percent.
